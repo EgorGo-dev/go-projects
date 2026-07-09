9ee1775e8bc2792e8670334e3a5855313f5cca36
Егор <gpp@gmail.com>
2026-07-09 18:30:56 +0300

    Добавил решение задачи 3

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 - [Задача 1](./work/task1/task1.md) — решение: [task1.go](./work/task1/task1.go)
 - [Задача 2](./work/task2/task2.md) — решение: [task2.go](./work/task2/task2.go)
+- [Задача 3](./work/task3/task3.md) — решение: [task3.go](./work/task3/task3.go)
--- work/task3/task3.go
+++ work/task3/task3.go
@@ -1,62 +1,61 @@
 package main
 
-import( 
-    "time"
-    "strings"
-    "errors"
-    "unicode/utf8"
+import (
+	"errors"
+	"strings"
+	"time"
+	"unicode/utf8"
 )
 
 func currentDayOfTheWeek() string {
-    now := TimeNow()
-    switch now.Weekday() {
-    case time.Monday:
-        return "Понедельник"
-    case time.Tuesday:
-        return "Вторник"
-    case time.Wednesday:
-        return "Среда"
-    case time.Thursday:
-        return "Четверг"
-    case time.Friday:
-        return "Пятница"
-    case time.Saturday:
-        return "Суббота"
-    case time.Sunday:
-        return "Воскресенье"
-    }
-    return ""
+	now := time.Now() // В редакторе LMS надо заменить на TimeNow()
+	switch now.Weekday() {
+	case time.Monday:
+		return "Понедельник"
+	case time.Tuesday:
+		return "Вторник"
+	case time.Wednesday:
+		return "Среда"
+	case time.Thursday:
+		return "Четверг"
+	case time.Friday:
+		return "Пятница"
+	case time.Saturday:
+		return "Суббота"
+	case time.Sunday:
+		return "Воскресенье"
+	}
+	return ""
 }
 
 func dayOrNight() string {
-    now := TimeNow()
-    hour := now.Hour()
-    if hour >= 10 && hour <= 22 {
-        return "День"
-    }
-    return "Ночь"
+	now := time.Now() // В редакторе LMS надо заменить на TimeNow()
+	hour := now.Hour()
+	if hour >= 10 && hour <= 22 {
+		return "День"
+	}
+	return "Ночь"
 }
 
-
 func nextFriday() int {
-    now := TimeNow()
+	now := time.Now()           // В редакторе LMS надо заменить на TimeNow()
 	today := int(now.Weekday()) // число от 0 до 6
-    if today == 5 {
-        return 0
-    }
-    days := (5 - today + 7) % 7
-    return days
+	if today == 5 {
+		return 0
+	}
+	days := (5 - today + 7) % 7
+	return days
 }
 
 func CheckCurrentDayOfTheWeek(answer string) bool {
-    currentDay := currentDayOfTheWeek()
-    return strings.EqualFold(answer, currentDay)
+	currentDay := currentDayOfTheWeek()
+	return strings.EqualFold(answer, currentDay)
 }
 
 func CheckNowDayOrNight(answer string) (bool, error) {
-    if utf8.RuneCountInString(answer) != 4 {
-        return false, errors.New("исправь свой ответ, а лучше ложись поспать")
-    }
-    boolean := strings.EqualFold(answer, dayOrNight())
-        return boolean, nil 
+	if utf8.RuneCountInString(answer) != 4 {
+		return false, errors.New("исправь свой ответ, а лучше ложись поспать")
+	}
+	boolean := strings.EqualFold(answer, dayOrNight())
+	return boolean, nil
 }
