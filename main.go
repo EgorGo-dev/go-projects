269144c209c6f1d968f8900b9f2e9f9ce5ca609e
Егор <gpp@gmail.com>
2026-08-15 12:25:43 +0300

    Улучшил дизаин

--- main.go
+++ main.go
@@ -1,38 +1,24 @@
-package main
+package main 
 
 import (
 	"fmt"
+	"net/http"
+	"log"
 )
 
-// TODO: bubbleSort без использования built-in sort
-func bubbleSort(nums []int) []int {
-	// Создаём копию среза, чтобы не менять оригинал
-	result := make([]int, len(nums))
-	copy(result, nums)
-
-	n := len(result)
-	if n <= 1 {
-		return result
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
 	}
-
-	swapped := true
-	for i := 0; i < n-1 && swapped; i++ {
-		swapped = false
-		for j := 0; j < n-i-1; j++ {
-			if result[j] > result[j+1] {
-				// Меняем местами
-				result[j], result[j+1] = result[j+1], result[j]
-				swapped = true
-			}
-		}
-	}
-	return result
+	fmt.Fprintf(w, "Ok let`s Go")
 }
 
-
-
 func main() {
-    fmt.Println(bubbleSort([]int{5, 3, 1, 4, 2})) // → [1, 2, 3, 4, 5]
-    fmt.Println(bubbleSort([]int{10, 9, 8}))       // → [8, 9, 10]
-    fmt.Println(bubbleSort([]int{1}))               // → [1]      // → 0
+	http.HandleFunc("/", handle)
+	fmt.Println("Сервер запущен на http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Ошибка при запуске сервера %v", err)
+	}
 }
